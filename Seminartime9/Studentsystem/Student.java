f18d5dc579becf3710f4f9b38ed4f04b1ce3cc65
Fridtjof Nystrøm <fridtjof@ramsvigtech.com>
2016-10-27 10:09:41 +0200

    Filer som brukes i timen

---
+++ Seminartime9/Studentsystem/Student.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+public class Student{
+
+	private String navn;
+	private HashMap<String, Fag> fagliste = new HashMap<String,Fag>();
+
+	public Student(String navn){
+		this.navn = navn;
+	}
+
+	public String toString(){
+		return navn;
+	}
+
+	public boolean finnesFagFraFoer(Fag f){
+		return fagliste.containsKey(f.toString());
+	}
+
+	public void leggTilFag(Fag f){
+		if(finnesFagFraFoer(f)){
+			System.out.println("Studenten tar allerede dette faget.");
+			return;
+		}
+		System.out.println("La til faget " + f.toString() + " i " + navn + " sin liste.");
+		fagliste.put(f.toString(),f);
+	}
+
+	public void fjernFag(Fag f){
+		if(!finnesFagFraFoer(f)){
+			System.out.println("Studenten tar ikke dette faget.");
+			return;
+		}
+		System.out.println("Fjernet faget " + f.toString() + " fra " + navn + " sin liste.");
+		fagliste.remove(f.toString());
+	}
+
+
+	public void skrivUtMineFag(){
+		for(String s : fagliste.keySet()){
+			System.out.println(s);
+		}
+	}
+
+	public void fjernMegFraAlleFag(){
+		for(Fag f : fagliste.values()){
+			f.fjernStudent(this);
+		}
+	}
+
+	public void fjernAlleMineFag(){
+		fagliste.clear();
+	}
+
+	public int antallFag(){
+		return fagliste.size();
+	}
+}
---
+++ Seminartime9/Studentsystem/Testfil.java
@@ -0,0 +1,65 @@
+public class Testfil{
+	public static void main(String[] args) {
+		Student testperson1 = new Student("Gunnar");
+		Fag testfag1 = new Fag("INF1000");
+		/* Tester Person.java */
+		
+		/*
+
+		System.out.println("\nTester å legge til et fag.");
+		testperson1.leggTilFag(testfag1);
+
+		System.out.println("\nTester å legge til samme fag på nytt.");
+		testperson1.leggTilFag(testfag1);
+
+		System.out.println("\nTester å fjerne faget fra studenten.");
+		testperson1.fjernFag(testfag1);
+	
+		System.out.println("\nTester å fjerne samme fag på nytt.");
+		testperson1.fjernFag(testfag1);
+	 	*/
+
+	 	if(testFjernAlle(testperson1)){
+	 		System.out.println("Det gaar a fjerne alle emnekoder fra student.");
+	 	}
+	 	testSkrivUt(testperson1);
+		/* Tester Fag.java */
+		/*
+		System.out.println("\nTester å legge til en person.");
+		testfag1.leggTilStudent(testperson1);
+
+		System.out.println("\nTester å legge til samme person på nytt.");
+		testfag1.leggTilStudent(testperson1);
+
+		System.out.println("\nTester å fjerne studenten fra faget.");
+		testfag1.fjernStudent(testperson1);
+	
+		System.out.println("\nTester å fjerne samme student på nytt.");
+		testfag1.fjernStudent(testperson1);
+		*/
+	}	
+
+	public static void testSkrivUt(Student s){
+		s.leggTilFag(new Fag("INF1060"));
+		s.leggTilFag(new Fag("INF2220"));
+		s.leggTilFag(new Fag("INF1600"));
+		s.skrivUtMineFag();
+	}
+
+	// Returnerer true hvis testen gikk fint.
+	public static boolean testFjernAlle(Student s){
+		s.leggTilFag(new Fag("INF1010"));
+		s.leggTilFag(new Fag("INF2220"));
+		s.fjernAlleMineFag();
+
+		/* //Overfloedig kode.
+		if(s.antallFag() == 0){
+			return true;
+		} else {
+			return false;
+		} */
+		return s.antallFag() == 0;
+
+	}
+
+}
